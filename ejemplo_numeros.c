029443109c918d684526fb297d88a151628f09f8
PofOverflow <ezequielstanganelli@gmail.com>
2021-08-18 09:52:02 -0300

    comentarios agregados en ejemplo_numeros

--- ejemplo_numeros.c
+++ ejemplo_numeros.c
@@ -5,15 +5,16 @@
 #define N 10
 #define MAX 25
 #define MIN 0
+#define AVG(x,y) ((float)x/y)
 
 enum booleano { VERDADERO = 0, FALSO = 1 };
 
 int es_mayor(int nums[N]);
 int es_minimo(int nums[N]);
+void es_min_max(int nums[N], int* min, int* max);
 float cal_promedio(int nums[N]);
 
 int main() {
-
     srand(time(NULL));
 
     int i = 0, numeros[N];
@@ -22,19 +23,33 @@ int main() {
 
     for(i = 0; i < N; i++) {
         numeros[i] = (rand()% MAX) - MIN;
-        printf("%d - ", numeros[i]);
+        printf("%d", numeros[i]);
+        if(i+1 < N) printf(" - ");
     }
+
     printf("\n");
     max_val = es_mayor(numeros);
     min_val = es_minimo(numeros);
+    /*Paso dirección de memoria de min_val y max_val
+    Esto me va a permitir retornar ambos valores
+    de la funcion es_min_max()*/
+    es_min_max(numeros, &min_val, &max_val);
     promedio = cal_promedio(numeros);
     printf("\nEl Valor Maximo es: %d", max_val);
     printf("\nEl Valor Minimo es: %d", min_val);
+    printf("\nMinimo [ %d ] - Maximo [ %d ]", min_val, max_val);
     printf("\nEl Valor Promedio es: %.2f", promedio);
 
     return 0;
 }
 
+/**
+* Función: es_maximo
+*--------------------
+* Retorna el valor maximo
+* nums: arreglo de numeros tamaño N
+* Return: int
+*/
 int es_mayor(int nums[N]) {
     int i = 0, maximo = -1;
 
@@ -47,6 +62,13 @@ int es_mayor(int nums[N]) {
     return maximo;
 }
 
+/**
+* Función: es_minimo
+*--------------------
+* Retorna el valor minimo
+* nums: arreglo de numeros tamaño N
+* Return: int
+*/
 int es_minimo(int nums[N]) {
     int i = 0, minimo = -1;
 
@@ -59,15 +81,45 @@ int es_minimo(int nums[N]) {
     return minimo;
 }
 
+/**
+* Función: es_min_max
+*--------------------
+* Retorna los valores minimos y máximos como PUNTEROS
+*
+* nums: arreglo de numeros tamaño N
+* min: valor puntero y retorna como puntero
+* max: valor puntero y retorna como puntero
+*
+* Return: void
+*/
+void es_min_max(int nums[N], int* min, int* max) {
+    int i = 0, min_ = -1, max_ = -1;
 
+    {
+        max = nums[i];
+        min = nums[i];
+    }
+
+    for(i = 1; i < N; i++) {
+        if(nums[i] > max)
+            max = nums[i];
+        if(nums[i] < min)
+            min = nums[i];
+    }
+}
+
+/**
+* Función: cal_promedio
+*--------------------
+* Retorna el retorna el promedio del array dado
+* nums: arreglo de numeros tamaño N
+* Return: float
+*/
 float cal_promedio(int nums[N]) {
-    float promedio = 0;
     int i = 0, suma = 0;
 
     for(i = 0; i < N; i++)
         suma += nums[i];
 
-    promedio = ((float)suma/(i+1));
-
-    return promedio;
+    return AVG(suma, (i+1));
 }
